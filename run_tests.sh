61ce336c8a921b1d4c6120ea339137a34bfb51d0
Joshua Tigno <tignojoshua@plmun.edu.ph>
2025-02-17 20:35:41 +0800

    Initial Project Files

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the Python program and test all input/output pairs in the tests folder
-python_script="switch_ternary_operator.py"
+python_script="function_arguments_return.py"
 
 declare -A results
 
